4fd55bbcbcd769af24384bca207e46c8614dae37
pcaiozzo <paigecaiozzo@gmail.com>
2022-06-28 01:43:45 -0400

    added votes table

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const db = require("../../db/connection");
 const inputCheck = require("../../utils/inputCheck");
 
+// Get all voters alphabetized by last name
 router.get("/voters", (req, res) => {
-  const sql = `SELECT * FROM voters`;
+  const sql = `SELECT * FROM voters ORDER BY last_name`;
 
   db.query(sql, (err, rows) => {
     if (err) {
@@ -19,7 +20,7 @@ router.get("/voters", (req, res) => {
 });
 
 // Get single voter
-router.get('/voter/:id', (req, res) => {
+router.get("/voter/:id", (req, res) => {
   const sql = `SELECT * FROM voters WHERE id = ?`;
   const params = [req.params.id];
 
@@ -29,12 +30,37 @@ router.get('/voter/:id', (req, res) => {
       return;
     }
     res.json({
-      message: 'success',
-      data: row
+      message: "success",
+      data: row,
     });
   });
 });
 
+// Create a voter
+router.post("/voter", ({ body }, res) => {
+  // Data validation
+  const errors = inputCheck(body, "first_name", "last_name", "email");
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.email];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+    });
+  });
+});
+
+// Update a voter's email
 router.put("/voter/:id", (req, res) => {
   // Data validation
   const errors = inputCheck(req.body, "email");
@@ -63,6 +89,7 @@ router.put("/voter/:id", (req, res) => {
   });
 });
 
+// Delete a voter
 router.delete("/voter/:id", (req, res) => {
   const sql = `DELETE FROM voters WHERE id = ?`;
 
